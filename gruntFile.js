8dfaadde622c02d028ff89c36376a25e9d5d9401
Josh <chaozhang78@icloud.com>
2017-10-04 22:13:02 -0400

    Added gruntFile to process files.

--- gruntFile.js
+++ gruntFile.js
@@ -1,24 +1,91 @@
 module.exports = function (grunt) {
 
-    // Project configuration.
+    // ===========================================================================
+    // CONFIGURE GRUNT ===========================================================
+    // ===========================================================================
     grunt.initConfig({
         distdir: 'dist',
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
+        banner:
+        '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? " * " + pkg.homepage + "\\n" : "" %>' +
+        ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;\n' +
+        '  */\n',
+        src: {
+            js: ['src/**/*.js', '!src/**/*test.js']
+        },
+        // configure jshint to validate js files -----------------------------------
+        jshint: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                reporter: require('jshint-stylish')
             },
-            build: {
-                src: 'src/app/app.js',
-                dest: 'build/<%= pkg.name %>.min.js'
+            all: ['Grunfile.js', 'src/**/*.js']
+        },
+        clean: ['<%= distdir %>/*'],
+        copy: {
+            html: {
+                cwd: 'src/app',  // set working folder / root to copy
+                src: '**/*.html',      // copy all files and subfolders **with ending .html**
+                dest: 'dist/app',    // destination folder
+                expand: true           // required when using cwd
+            },
+            assets: {
+                cwd: 'src/assets',  // set working folder / root to copy
+                src: '**/*.*',      // copy all files and subfolders **with ending .html**
+                dest: 'dist/assets',    // destination folder
+                expand: true           // required when using cwd
+            }
+        },
+        concat: {
+            dist: {
+                options: {
+                    banner: "<%= banner %>"
+                },
+                src: ['<%= src.js %>'],
+                dest: '<%= distdir %>/app.js'
+            },
+            index: {
+                src: ['src/index.html'],
+                dest: '<%= distdir %>/index.html',
+                options: {
+                    process: true
+                }
+            },
+            angular: {
+                src: ['node_modules/angular/angular.js', 'node_modules/angular-route/angular-route.js'],
+                dest: '<%= distdir %>/angular.js'
+            }
+        },
+        uglify: {
+            dist: {
+                options: {
+                    banner: "<%= banner %>"
+                },
+                src: ['<%= src.js %>'],
+                dest: '<%= distdir %>/app.js'
+            },
+            angular: {
+                src: ['<%= concat.angular.src %>'],
+                dest: '<%= distdir %>/angular.js'
             }
         }
     });
 
-    // Load the plugin that provides the "uglify" task.
+    // ===========================================================================
+    // LOAD GRUNT PLUGINS ========================================================
+    // ===========================================================================
+    // we can only load these if they are in our package.json
+    // make sure you have run npm install so our app can find these
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['clean', 'copy', 'concat']);
 
 };
