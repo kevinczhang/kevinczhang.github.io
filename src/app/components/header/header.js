dba232f99461b647bae993fe7d3903284de4cd79
Josh <chaozhang78@icloud.com>
2017-10-24 22:30:02 -0400

    Edited about, project and header.

--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -3,13 +3,28 @@
     var appHeader = angular.module('myApp.header', []);
 
     appHeader.controller("headerCtrl", ['$scope', function ($scope) {
-            
-        }]);
+        $scope.items = [
+            'The first choice!',
+            'And another choice for you.',
+            'but wait! A third!'
+        ];
+        $scope.status = {
+            isopen: false
+        };
+        $scope.toggled = function (open) {
+            $log.log('Dropdown is now: ', open);
+        };
+        $scope.toggleDropdown = function ($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.status.isopen = !$scope.status.isopen;
+        };
+    }]);
 
-    myApp.directive("siteHeader",function(){
+    myApp.directive("siteHeader", function () {
         return {
             restrict: 'A',
-            templateUrl:'app/components/header/header.html'
+            templateUrl: 'app/components/header/header.html'
         };
     });
 
