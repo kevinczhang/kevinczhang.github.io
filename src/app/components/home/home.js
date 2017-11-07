56d0658200d69cb791c70e3b2efaaa507b0964ba
Josh <chaozhang78@icloud.com>
2017-11-06 22:18:23 -0500

    Modified less and use controller as syntax.

--- src/app/components/home/home.js
+++ src/app/components/home/home.js
@@ -10,25 +10,22 @@
       });
     }])
 
-    .controller('HomeCtrl', function ($scope) {
-      $scope.myInterval = 5000;
-      $scope.noWrapSlides = false;
-      $scope.active = 0;
-      var slides = $scope.slides = [];
+    .controller('HomeCtrl', function () {
+      this.myInterval = 5000;
+      this.noWrapSlides = false;
+      this.active = 0;
+      var slides = this.slides = [];
       var currIndex = 0;
+      this.fullName = 'Chao Zhang';
+      this.title = 'Full Stack Developer';
 
-      $scope.addSlide = function () {
+      this.addSlide = function () {
         var newWidth = 600 + slides.length + 1;
         slides.push({
           image: 'http://unsplash.it/' + newWidth + '/300',
           text: ['Nice image', 'Awesome photograph', 'That is so cool', 'I love that'][slides.length % 4],
           id: currIndex++
         });
-      };
-
-      $scope.randomize = function () {
-        var indexes = generateIndexesArray();
-        assignNewIndexesToSlides(indexes);
       };
 
       slides.push({
@@ -42,39 +39,7 @@
           id: currIndex++
         });
       for (var i = 2; i < 4; i++) {
-        $scope.addSlide();
-      }
-
-      // Randomize logic below
-
-      function assignNewIndexesToSlides(indexes) {
-        for (var i = 0, l = slides.length; i < l; i++) {
-          slides[i].id = indexes.pop();
-        }
-      }
-
-      function generateIndexesArray() {
-        var indexes = [];
-        for (var i = 0; i < currIndex; ++i) {
-          indexes[i] = i;
-        }
-        return shuffle(indexes);
-      }
-
-      // http://stackoverflow.com/questions/962802#962890
-      function shuffle(array) {
-        var tmp, current, top = array.length;
-
-        if (top) {
-          while (--top) {
-            current = Math.floor(Math.random() * (top + 1));
-            tmp = array[current];
-            array[current] = array[top];
-            array[top] = tmp;
-          }
-        }
-
-        return array;
+        this.addSlide();
       }
     });
 } (angular));
